d39025bee0b3dc46fdaf1b4e37c3d25ebd1e4ca5
evanfang0054 <380616679@qq.com>
2025-03-19 19:56:10 +0800

    feat: 添加 Turbo 和 Changesets 支持，更新 package.json 脚本，新增多个子包的 package.json 文件

---
+++ packages/browser/README.md
@@ -0,0 +1,7 @@
+# 浏览器端性能监控
+
+## 安装
+
+```bash
+pnpm add @sentinel/browser
+```
---
+++ packages/browser/src/index.ts
@@ -0,0 +1,7 @@
+const browser = {
+  name: 'browser',
+  version: '0.0.1',
+  description: '浏览器端性能监控',
+}
+
+export default browser
