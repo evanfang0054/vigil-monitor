6d8058ab6d0a8e3186160074a5187e51d717c146
evanfang0054 <380616679@qq.com>
2025-04-16 20:52:09 +0800

    chore: 删除不再使用的浏览器、核心、React、共享、类型、工具和Vue包的相关文件，优化项目结构

--- packages/browser/README.md
+++
@@ -1,7 +0,0 @@
-# 浏览器端性能监控
-
-## 安装
-
-```bash
-pnpm add @vigil-monitor/browser
-```
--- packages/browser/src/index.ts
+++
@@ -1,7 +0,0 @@
-const browser = {
-  name: 'browser',
-  version: '0.0.1',
-  description: '浏览器端性能监控',
-};
-
-export default browser;
